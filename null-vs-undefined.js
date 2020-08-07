02431e5545322dd0024eca579af5f9f7e07a59c8
monna p hero <moheuddin.monnab4u@gmail.com>
2020-08-07 16:14:08 +0800

    null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,17 @@
+//node null-vs-undefined.js
+
+// function add(num1 ,num2){
+//     console.log(num1+num2);
+// }
+
+// const sum=add(12,15);
+// console.log(sum);
+
+const person={name:"MOnna" , Phonne : 15536830331 };
+console.log(person.gf)
+
+const fun= undefined;
+console.log(fun);
+
+const age=[13,39,29];
+console.log(age[11]);
