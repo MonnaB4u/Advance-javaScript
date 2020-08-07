15a514a72780b419b07add819971061c54352bde
monna p hero <moheuddin.monnab4u@gmail.com>
2020-08-07 15:49:01 +0800

    true false

---
+++ advjs.js
@@ -0,0 +1,16 @@
+//falsy
+//0
+//NAN
+//uindefine
+//null
+// " "
+//////////true////
+//  '0' , [], ' '
+
+const num=[];
+if(num){
+    console.log("your condition is true");
+}else{
+
+    console.log("your condition is false");
+}
